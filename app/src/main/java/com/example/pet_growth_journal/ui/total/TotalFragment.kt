b97fb0038cc970a77c465fc1bb16abcecad42384
hyunwook <c004112@gmail.com>
2022-10-17 00:56:39 +0900

    chore: base set on totalview

--- app/src/main/java/com/example/pet_growth_journal/ui/total/TotalFragment.kt
+++ app/src/main/java/com/example/pet_growth_journal/ui/total/TotalFragment.kt
@@ -6,35 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.pet_growth_journal.databinding.FragTotalBinding
 
 class TotalFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: TotalViewModel
     private var _binding: FragTotalBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val totalViewModel: TotalViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(TotalViewModel::class.java)
 
-        _binding = FragTotalBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        _binding = FragTotalBinding.inflate(inflater, container, false).apply {
+            viewModel = totalViewModel
+            lifecycleOwner = this@TotalFragment
+        }
+       return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/pet_growth_journal/ui/total/TotalViewModel.kt
+++ app/src/main/java/com/example/pet_growth_journal/ui/total/TotalViewModel.kt
@@ -1,16 +1,13 @@
 package com.example.pet_growth_journal.ui.total
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.pet_growth_journal.ui.dailygrow.Category
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class TotalViewModel : ViewModel() {
+@HiltViewModel
+class TotalViewModel @Inject constructor() : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
 }
 
 data class CommonTotalModel(
