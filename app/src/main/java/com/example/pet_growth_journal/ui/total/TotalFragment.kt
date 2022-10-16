56381a6831c1838a117e0a15d585a26e7f03802a
hyunwook <c004245@gmail.com>
2022-10-17 01:19:00 +0900

    chore: test commit

--- app/src/main/java/com/example/pet_growth_journal/ui/total/TotalFragment.kt
+++ app/src/main/java/com/example/pet_growth_journal/ui/total/TotalFragment.kt
@@ -23,9 +23,7 @@ class TotalFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-
+    ): View {
         _binding = FragTotalBinding.inflate(inflater, container, false).apply {
             viewModel = totalViewModel
             lifecycleOwner = this@TotalFragment
