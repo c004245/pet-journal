d15c0a4099e10f9ff63547c0f0ab1ee6692c6aa1
mapppppppp <c004245@naver.com>
2023-02-05 21:44:34 +0900

    chore: ui register pet name

--- app/src/main/java/com/example/pet_growth_journal/ui/onboarding/register/RegisterNameFragment.kt
+++ app/src/main/java/com/example/pet_growth_journal/ui/onboarding/register/RegisterNameFragment.kt
@@ -1,9 +1,37 @@
 package com.example.pet_growth_journal.ui.onboarding.register
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.pet_growth_journal.databinding.FragRegisterNameBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RegisterNameFragment: Fragment() {
 
-    private var
+    private var _binding: FragRegisterNameBinding? = null
+    private val binding get() = _binding!!
+
+    private val registerViewModel: RegisterViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragRegisterNameBinding.inflate(inflater, container, false).apply {
+            viewModel = registerViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
