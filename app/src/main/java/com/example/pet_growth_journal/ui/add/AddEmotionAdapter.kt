3a17c800c6ac19b7d4bfde124dce32e14eca9799
c004245 <c004245@naver.com>
2023-06-11 16:04:14 +0900

    chore: add emotion icon

---
+++ app/src/main/java/com/example/pet_growth_journal/ui/add/AddEmotionAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.pet_growth_journal.ui.add
+
+class AddEmotionAdapter(
+    private val context: Context,
+    val onClickEmotion: ()
+)
+}
+
+
+data class AddEmotionModel(
+    val id: Int
+    val icon: Int,
+    val se
+)
